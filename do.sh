34ca6e5a44aa48eb7438cba891bd55e1bbf88435
Your Name <you@example.com>
2024-04-23 21:43:35 +0200

    First Commit

---
+++ do.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+case $1 in
+    build )
+        shift
+        eval "docker compose build $@"
+    ;;
+    stop | down )
+        docker compose down
+    ;;
+    start | up )
+        docker compose up -d
+    ;;
+    logs )
+        docker logs -f ssh-tor
+    ;;
+    onion )
+        docker exec -it ssh-tor cat /var/lib/tor/torssh/hostname
+    ;;
+esac
---
+++ tor/restart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+echo started tor for ssh/http
+
+# echo "## Set permissions on /var/lib/tor dir ###"
+# chmod 750 /var/lib/tor
+
+echo "## Starting tor #############################"
+
+tor_options=(
+  --SocksPort 127.0.0.1:9050
+  # --SocksPolicy "accept 192.168.0.0/24"
+  # --SocksPolicy "reject *"
+  # --DataDirectory /var/lib/tor/.tor
+  --DataDirectoryGroupReadable 1
+  --HiddenServiceDir /var/lib/tor/torssh
+  --HiddenServicePort "22 127.0.0.1:22"
+  # --HiddenServiceDir /var/lib/tor/http
+  # --HiddenServicePort "80 127.0.0.1:80"
+)
+
+exec tor "${tor_options[@]}"
